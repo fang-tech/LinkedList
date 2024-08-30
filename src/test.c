5b8e14bc7d7eab7aa86d8d9e108d69b8f7ea9a3a
Jiu Yu <3207248497@qq.com>
2024-08-31 00:30:47 +0800

    头插法和尾插法函数及其测试文件的更新

--- src/test.c
+++ src/test.c
@@ -4,6 +4,28 @@
     @author:    func
     @brief:     编写测试函数的地方, 测试自己写的功能无误, 每个操作都需要写一个测试函数
 */
+void test_push_front(){
+    push_front(&head1,1);
+    push_back(&head1, 2);
+    push_back(&head1, 3);
+     printf("应该输出 3 2 1\n");
+    printf("实际输出 \n");
+    printList(head1); 
+
+    destroyList(&head1);
+}
+
+void test_push_back(){
+    push_front(&head1,1);
+    push_back(&head1, 2);
+    push_back(&head1, 3);
+     printf("应该输出 1 2 3\n");
+    printf("实际输出 \n");
+    printList(head1); 
+
+    destroyList(&head1);
+}
+
 void test_printList() {
     Node* head = NULL;
 
@@ -36,6 +58,8 @@ void test_destroyList() {
 }
 
 int main(){
+    test_push_front();
+    test_push_back();
     test_printList();
     test_destroyList();
 
